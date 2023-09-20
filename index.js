50bdea8fd075de73b1b8c3e453c00e57c87fa061
Joan Macharia <joan.macharia@students.moringa>
2023-09-20 11:15:25 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = (num1, num2) => {
+    return 2000 / 100
+}
+console.log (divide(2000,100))
+
+
+    
+const square = (x) => {
+    return  x ** 2
+}
+console.log (square(2))
+
+
+
+const add = (param1, param2) => {
+    return param1 + param2
+}
+console.log (add(3, 4))
